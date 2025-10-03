221b7d7face1982b874bdb83eb0ae4ba584fc86b
Hk113258 <154900114+Hk113258@users.noreply.github.com>
2025-10-03 21:03:14 +0500

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,10 +1,10 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~O5JhiZBT#wmOlvLwyVuCA5LpT4kcuN_N5i0CS-I6PYyPY2vVG0vg55e0ce18e09b12a0",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "923288744355", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
-  TIMEZONE: "Africa/Harare" //put your country timeZone....leave blank if u don't know.
+  TIMEZONE: "PAKISTAN/MANSHERA" //put your country timeZone....leave blank if u don't know.
 };
